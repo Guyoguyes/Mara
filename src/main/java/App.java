ac9f8b4f7df1dedd42bd875d0a3c0e7cecac9517
Guyoguyes <g.abuba43@gmail.com>
2020-06-08 00:31:56 +0300

    home page route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,24 @@
 import java.util.HashMap;
 import java.util.Map;
 
+import DB.DB;
+import org.sql2o.Sql2o;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import spark.ModelAndView;
 import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
+        staticFileLocation("/public");
 
+        //Database
+        String connection = "jdbc:postgresql://localhost:5432/wildlife-tracker";
+        Sql2o sql2o = new Sql2o(connection , "guyo", "@#scorpion  ");
+
+        //get home page
+        get("/", (req, res) ->{
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
