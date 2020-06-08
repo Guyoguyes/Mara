e59d69ecf2b93cddfc6242a33edfa349cff4ba31
Guyoguyes <g.abuba43@gmail.com>
2020-06-08 09:37:40 +0300

    Ranger post and get

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,37 +1,49 @@
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//import DB.DB;
-//import org.sql2o.Sql2o;
-//import spark.template.handlebars.HandlebarsTemplateEngine;
-//import spark.ModelAndView;
-//import static spark.Spark.*;
-//
-//public class App {
-//    public static void main(String[] args) {
-//        staticFileLocation("/public");
-//
-//        //Database
-//        String connection = "jdbc:postgresql://localhost:5432/wildlife-tracker";
-//        Sql2o sql2o = new Sql2o(connection , "guyo", "@#scorpion");
-//
-//        //get home page
-//        get("/", (req, res) ->{
-//            Map<String, Object> model = new HashMap<>();
-//            return new ModelAndView(model, "index.hbs");
-//        }, new HandlebarsTemplateEngine());
-//
-//        //get animal form
-//        get("/animal-form", (req, res) ->{
-//            Map<String, Object> model = new HashMap<>();
-//            return new ModelAndView(model, "animal-form");
-//        }, new HandlebarsTemplateEngine());
-//
-//        //post animal info
-//        post("/animals", (req, res) ->{
-//            Map<String, Object> model = new HashMap<>();
-//            String dName = req.queryParams("name");
-//
-//        })
-//    }
-//}
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import DB.DB;
+import models.Animal;
+import models.Ranger;
+import org.sql2o.Sql2o;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import spark.ModelAndView;
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        //Database
+        String connection = "jdbc:postgresql://localhost:5432/wildlife-tracker";
+        Sql2o sql2o = new Sql2o(connection , "guyo", "@#scorpion");
+
+        //get home page
+        get("/", (req, res) ->{
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //get form
+        get("/ranger/form", (req, res) ->{
+            Map<String, Object> model = new HashMap<>();
+           return new ModelAndView(model, "ranger-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/ranger", (req, res) ->{
+            Map<String, Object> model = new HashMap<>();
+            String name = req.queryParams("name");
+            int budgeNo = Integer.parseInt(req.queryParams("budgeno"));
+            Ranger ranger = new Ranger(name, budgeNo);
+            model.put("ranger", ranger);
+            return new ModelAndView(model, "ranger.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/ranger", (req, res) ->{
+            Map<String, Object> model = new HashMap<>();
+            List<Ranger> rangers = Ranger.all();
+            model.put("rangers", rangers);
+            return new ModelAndView(model, "ranger.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
+}
